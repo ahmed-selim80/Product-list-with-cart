36865826e2086e70e268ba1c82bf6bb5f0f6a14b
Ahmed Selim <ahmedselimnoshi@gmail.com>
2025-09-16 03:43:34 +0300

    trying to fix the data.json 5

--- scripts/script.js
+++ scripts/script.js
@@ -26,7 +26,7 @@ const GetProducts = async function(url){
 // products is the array of all products
 // const jsonURL = URL.createObjectURL("../data.json")
 // console.log(jsonURL)
-const products = await GetProducts("Product-list-with-cart/data.json");
+const products = await GetProducts("https://ahmed-selim80.github.io/Product-list-with-cart/data.json");
 const productsInCart = [];
 
 // rendering the products
